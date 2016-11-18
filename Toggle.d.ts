a78366371cd0c3659c44f0cbf223b887d8ff7e74
Daniel Lytkin <gaelco.salmon@gmail.com>
2016-11-18 13:07:42 +0700

    Fix typings for `Toggle`

--- Toggle.d.ts
+++ Toggle.d.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 
-export interface ToggleProps extends React.Props<Toggle> {
+export interface ToggleProps {
   defaultToggled?: boolean;
   disabled?: boolean;
   elementStyle?: React.CSSProperties;
@@ -14,8 +14,10 @@ export interface ToggleProps extends React.Props<Toggle> {
   rippleStyle?: React.CSSProperties;
   style?: React.CSSProperties;
   thumbStyle?: React.CSSProperties;
+  thumbSwitchedStyle?: React.CSSProperties;
   toggled?: boolean;
   trackStyle?: React.CSSProperties;
+  trackSwitchedStyle?: React.CSSProperties;
 }
 
 export default class Toggle extends React.Component<ToggleProps, {}> {
