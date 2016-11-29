a1cfdd9b46fe4acf127b41b0a0600ca6031f16c9
Daniel Lytkin <gaelco.salmon@gmail.com>
2016-11-29 12:51:49 +0700

    Update typings for `TextField`

--- TextField.d.ts
+++ TextField.d.ts
@@ -37,6 +37,7 @@ export interface TextFieldProps {
   min?: string | number;
   max?: string | number;
   step?: string | number;
+  autoFocus?: boolean;
 }
 
 export default class TextField extends React.Component<TextFieldProps, {}> {
